e0150c594aa5e00a779f0023f210a33e3eb0892d
Lingling-Z <97594629+Lingling-Z@users.noreply.github.com>
2022-08-03 22:34:17 +1200

    scroll animation

---
+++ miniProjects/scroll_animation/script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll('.box')
+
+// checkBoxes()
+
+const checkBoxes = () => {
+  const triggerBottom = window.innerHeight * 0.8
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+  })
+}
+
+window.addEventListener('scroll', checkBoxes)
